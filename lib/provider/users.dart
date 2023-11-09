1d2176103cacdc63cb1090418964104f223efa3b
Lucas A. Santos <lucas_ads@hotmail.com.br>
2023-11-08 21:28:31 -0400

    Integração com o firebase

--- lib/provider/users.dart
+++ lib/provider/users.dart
@@ -1,12 +1,20 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/data/dummy_users.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/user.dart';
 
 class Users with ChangeNotifier {
-  final Map<String, User> _items = {...DUMMY_USERS};
+  static const _baseUrl = 'https://sua-url-do-firebase.firebaseio.com/';
+
+  final Map<String, User> _items = {};
+
+  Users() {
+    loadUsers();
+  }
 
   List<User> get all {
     return [..._items.values];
@@ -20,8 +28,17 @@ class Users with ChangeNotifier {
     return _items.values.elementAt(i);
   }
 
-  void put(User user) {
+  Future<void> put(User user) async {
     if (user.id.trim().isNotEmpty && _items.containsKey(user.id)) {
+      final response = await http.patch(
+        Uri.parse("$_baseUrl/users/${user.id}.json"),
+        body: json.encode({
+          'name': user.name,
+          'email': user.email,
+          'avatarUrl': user.avatarUrl
+        }),
+      );
+
       _items.update(
           user.id,
           (param) => User(
@@ -30,7 +47,17 @@ class Users with ChangeNotifier {
               email: user.email,
               avatarUrl: user.avatarUrl));
     } else {
-      final id = Random().nextDouble().toString();
+      final response = await http.post(
+        Uri.parse("$_baseUrl/users.json"),
+        body: json.encode({
+          'name': user.name,
+          'email': user.email,
+          'avatarUrl': user.avatarUrl
+        }),
+      );
+
+      final id = json.decode(response.body)['name'];
+
       _items.putIfAbsent(
           id,
           () => User(
@@ -42,10 +69,34 @@ class Users with ChangeNotifier {
     notifyListeners();
   }
 
-  void remove(User user) {
+  Future<void> remove(User user) async {
+    final response =
+        await http.delete(Uri.parse("$_baseUrl/users/${user.id}.json"));
+
     if (user.id != null) {
       _items.remove(user.id);
       notifyListeners();
     }
+  }
+
+  Future<void> loadUsers() async {
+    final response = await http.get(
+      Uri.parse("$_baseUrl/users.json"),
+    );
+
+    var jsonUsers = json.decode(response.body) as Map<String, dynamic>;
+
+    _items.addAll(jsonUsers.map((key, value) {
+      return MapEntry(
+          key,
+          User(
+            id: key,
+            name: value['name'] as String,
+            email: value['email'] as String,
+            avatarUrl: value['avatarUrl'] as String,
+          ));
+    }));
+
+    notifyListeners();
   }
 }
--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -3,11 +3,18 @@ import 'package:flutter_crud/models/user.dart';
 import 'package:flutter_crud/provider/users.dart';
 import 'package:provider/provider.dart';
 
-class UserForm extends StatelessWidget {
-  final _form = GlobalKey<FormState>();
-  final Map<String, String> _formData = {};
-
+class UserForm extends StatefulWidget {
   UserForm({super.key});
+
+  @override
+  State<UserForm> createState() => _UserFormState();
+}
+
+class _UserFormState extends State<UserForm> {
+  final _form = GlobalKey<FormState>();
+  bool _isLoading = false;
+
+  final Map<String, String> _formData = {};
 
   void _loadFormData(User user) {
     _formData['id'] = user.id;
@@ -32,12 +39,17 @@ class UserForm extends StatelessWidget {
         backgroundColor: Colors.blue,
         actions: [
           IconButton(
-              onPressed: () {
+              onPressed: () async {
                 final isValid = _form.currentState!.validate();
                 if (isValid) {
                   _form.currentState!.save();
                   print(_formData);
-                  Provider.of<Users>(context, listen: false).put(User(
+
+                  setState(() {
+                    _isLoading = true;
+                  });
+
+                  await Provider.of<Users>(context, listen: false).put(User(
                       id: _formData['id'] != null
                           ? _formData['id'] as String
                           : '',
@@ -45,46 +57,54 @@ class UserForm extends StatelessWidget {
                       email: _formData['email']!,
                       avatarUrl: _formData['avatarUrl']!));
                   Navigator.of(context).pop();
+
+                  setState(() {
+                    _isLoading = false;
+                  });
                 }
               },
               icon: Icon(Icons.save))
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(15),
-        child: Form(
-            key: _form,
-            child: Column(
-              children: [
-                TextFormField(
-                  initialValue: _formData['name'],
-                  decoration: InputDecoration(labelText: 'Nome'),
-                  validator: (value) {
-                    if (value == null || value!.trim().isEmpty) {
-                      return "Informe um nome...";
-                    }
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Padding(
+              padding: EdgeInsets.all(15),
+              child: Form(
+                  key: _form,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        initialValue: _formData['name'],
+                        decoration: InputDecoration(labelText: 'Nome'),
+                        validator: (value) {
+                          if (value == null || value!.trim().isEmpty) {
+                            return "Informe um nome...";
+                          }
 
-                    if (value.trim().length < 3) {
-                      return "Nome muito pequeno. No mínimo 3 letras";
-                    }
+                          if (value.trim().length < 3) {
+                            return "Nome muito pequeno. No mínimo 3 letras";
+                          }
 
-                    return null;
-                  },
-                  onSaved: (value) => _formData['name'] = value!,
-                ),
-                TextFormField(
-                  initialValue: _formData['email'],
-                  decoration: InputDecoration(labelText: 'E-mail'),
-                  onSaved: (value) => _formData['email'] = value!,
-                ),
-                TextFormField(
-                  initialValue: _formData['avatarUrl'],
-                  decoration: InputDecoration(labelText: 'URL do avatar'),
-                  onSaved: (value) => _formData['avatarUrl'] = value!,
-                )
-              ],
-            )),
-      ),
+                          return null;
+                        },
+                        onSaved: (value) => _formData['name'] = value!,
+                      ),
+                      TextFormField(
+                        initialValue: _formData['email'],
+                        decoration: InputDecoration(labelText: 'E-mail'),
+                        onSaved: (value) => _formData['email'] = value!,
+                      ),
+                      TextFormField(
+                        initialValue: _formData['avatarUrl'],
+                        decoration: InputDecoration(labelText: 'URL do avatar'),
+                        onSaved: (value) => _formData['avatarUrl'] = value!,
+                      )
+                    ],
+                  )),
+            ),
     );
   }
 }
